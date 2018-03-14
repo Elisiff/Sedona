363a5786bae2f40c42f949df096e71ffbc48becc
Anastasiya Vasina <nast791@mail.ru>
2018-03-14 19:35:59 +0300

    form validate intro part 1

--- js/script.js
+++ js/script.js
@@ -2,41 +2,62 @@
 
 (function () {
   var form = document.querySelector('.review-form');
-  var introInput = document.querySelectorAll('.form-intro__input');
+  var introWrap = document.querySelector('.form-intro__wrapper');
+  if (introWrap) {
+    var introLabel = introWrap.getElementsByTagName('label');
+  }
+  // var contactsTel = document.getElementById('form-contacts__tel-field');
+  // var contactsMail = document.getElementById('form-contacts__email-field');
 
-  // function validateIntro() {
-  //   for (var i = 0; i < 1; i++) {
-  //     if (introInput[i].required) {
-  //       if (introInput[i].value.length < 3) {
-  //         introInput[i].setCustomValidity('Имя или фамилия должны состоять минимум из 3 букв');
-  //         introInput[i].setAttribute('style', 'box-shadow: 0 0 0 1px red;');
-  //         return false;
-  //       } else {
-  //         introFieldValid();
-  //       }
-  //     } else {
-  //       introFieldValid();
-  //     }
-  //   }
-  // }
+  function validateIntro() {
+    if (introLabel) {
+      for (var i = 0; i < introLabel.length; i++) {
+        var introInput = introLabel[i].querySelector('.form-intro__input');
+        introInput.value = introInput.value.charAt(0).toUpperCase() + introInput.value.substr(1);
+
+        if (introInput.required) {
+          if (introInput.value.length < 2) {
+            introInput.setCustomValidity('Имя или фамилия должны состоять минимум из 2 букв');
+            introInput.setAttribute('style', 'box-shadow: 0 0 0 1px red;');
+            return false;
+          } else {
+            introFieldValid();
+          }
+        } else {
+          introFieldValid();
+        }
+      }
+    }
+    return validateIntro;
+  }
 
   function introFieldValid() {
-    introInput.setCustomValidity('');
-    introInput.setAttribute('style', 'box-shadow: none;');
+    if (introLabel) {
+      for (var i = 0; i < introLabel.length; i++) {
+        var introInput = introLabel[i].querySelector('.form-intro__input');
+        
+        introInput.setCustomValidity('');
+        introInput.setAttribute('style', 'box-shadow: none;');
+      }
+    }
   }
 
-  function validateContacts() {
+  // function validateTelField() {
 
-  }
-
-  // function onIntroFieldChange() {
-  //   for (var i = 0; i < 1; i++) {
-  //     introInput[i].addEventListener('blur', function () {
-  //       validateIntro();
-  //     });
-  //   }
   // }
-  // onIntroFieldChange();
+
+  function onIntroFieldChange() {
+    if (introLabel) {
+      for (var i = 0; i < introLabel.length; i++) {
+        var introInput = introLabel[i].querySelector('.form-intro__input');
+
+        introInput.addEventListener('blur', function () {
+          validateIntro();
+        });
+      }
+    }
+  }
+  onIntroFieldChange();
 })();
 
 'use strict';
