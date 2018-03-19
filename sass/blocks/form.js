ba08760cf37df42742f0d36034867c8a88325494
Anastasiya Vasina <nast791@mail.ru>
2018-03-19 19:42:43 +0300

    validate form part 3

--- sass/blocks/form.js
+++ sass/blocks/form.js
@@ -8,25 +8,28 @@
   }
   var telError = document.querySelector('.form-contacts__error');
   var contactsTel = document.getElementById('form-contacts__tel-field');
-  // var contactsMail = document.getElementById('form-contacts__email-field');
+  var contactsMail = document.getElementById('form-contacts__email-field');
 
   function validateIntro() {
     if (introLabel) {
       for (var i = 0; i < introLabel.length; i++) {
-        var introInput = introLabel[i].querySelector('.form-intro__input');
+        var introInput = introLabel[i].querySelector('.form-intro__input');    
+        introInput.value = introInput.value.trim();
         introInput.value = introInput.value.charAt(0).toUpperCase() + introInput.value.substr(1);
-        var introInputValue = (introInput.value).trim(); // удаляем пробелы в начале и конце строки
+        var introInputValue = introInput.value.trim(); // удаляем пробелы в начале и конце строки
         var introWords = introInputValue.split(/\s+/); // считаем 1 и более пробел за 1 пробел
 
         if (introInput.value !== '') {
           if (introWords.length > 1) {
             introInput.setCustomValidity('Имя, фамилия или отчество должны состоять из 1 слова');
             introInput.setAttribute('style', 'box-shadow: inset 0 0 0 2px red;');
+            introInput.style.outline = 'none';
             return false;
           } else if (introInput.required) {
             if (introInput.value.length < 2) {
               introInput.setCustomValidity('Имя или фамилия должны состоять минимум из 2 букв');
               introInput.setAttribute('style', 'box-shadow: inset 0 0 0 2px red;');
+              introInput.style.outline = 'none';
               return false;
             } else {
               introFieldValid();
@@ -34,6 +37,8 @@
           } else {
             introFieldValid();
           }
+        } else {
+          introFieldValid();
         }
       }
     }
@@ -47,6 +52,7 @@
         
         introInput.setCustomValidity('');
         introInput.setAttribute('style', 'box-shadow: none;');
+        introInput.setAttribute('style', 'outline: invert none medium;');
       }
     }
   }
@@ -59,11 +65,14 @@
         if (!reg.test(contactsTel.value)) {
           contactsTel.setCustomValidity('Номер телефона должен соответствовать шаблону: + 7 ХХХ ХХХ ХХ ХХ или 8 ХХХ ХХХ ХХ ХХ');
           contactsTel.setAttribute('style', 'box-shadow: inset 0 0 0 2px red;');
+          contactsTel.style.outline = 'none';
           telError.setAttribute('style', 'visibility: visible;');
           return false;
         } else {
           telFieldValid();
         }
+      } else {
+        telFieldValid();
       }
     }
     return validateTelField;
@@ -73,7 +82,36 @@
     if (contactsTel) {
       contactsTel.setCustomValidity('');
       contactsTel.setAttribute('style', 'box-shadow: none;');
+      contactsTel.setAttribute('style', 'outline: invert none medium;');
       telError.setAttribute('style', 'visibility: hidden;');
+    }
+  }
+
+  function validateMailField() {
+    if (contactsMail) {
+      var reg = /^[-._a-z0-9]+@(?:[a-z0-9][-a-z0-9]+\.)+[a-z]{2,6}$/gi;
+      
+      if (contactsMail.value !== '') {
+        if (!reg.test(contactsMail.value)) {
+          contactsMail.setCustomValidity('Адрес электронной почты должен соответствовать шаблону: X@XX.XX');
+          contactsMail.setAttribute('style', 'box-shadow: inset 0 0 0 2px red;');
+          contactsMail.style.outline = 'none';
+          return false;
+        } else {
+          mailFieldValid();
+        }
+      } else {
+        mailFieldValid();
+      }
+    }
+    return validateMailField;
+  }
+
+  function mailFieldValid() {
+    if (contactsMail) {
+      contactsMail.setCustomValidity('');
+      contactsMail.setAttribute('style', 'box-shadow: none;');
+      contactsMail.setAttribute('style', 'outline: invert none medium;');
     }
   }
 
@@ -98,4 +136,13 @@
     }
   }
   onTelFieldChange();
+
+  function onMailFieldChange() {
+    if (contactsMail) {
+      contactsMail.addEventListener('blur', function () {
+        validateMailField();
+      });
+    }
+  }
+  onMailFieldChange();
 })();
